438543b9ebe42277fa46a275a3f267dd53d80a8f
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-04-02 22:59:00 +0330

    can update notebooks

--- src/pages/notebook-details.tsx
+++ src/pages/notebook-details.tsx
@@ -1,16 +1,43 @@
 import { Button, TextInput, Textarea } from "@mantine/core";
 import { useParams, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { useNotebooksStore } from "../store";
 
 const NotebookDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
+
   const allNotebooks = useNotebooksStore((s) => s.notebooks);
   const notebook = allNotebooks.find((n) => n.id === params.id);
-  const [content, setContent] = useState(notebook?.content ?? "");
-  const [title, setTitle] = useState(notebook?.title ?? "");
+  const [initialTitle, setInitialTitle] = useState(notebook?.title ?? "");
+  const [initialContent, setInitialContent] = useState(notebook?.content ?? "");
+
+  const [title, setTitle] = useState(initialTitle);
+  const [content, setContent] = useState(initialContent);
+
+  let [isSaveButtonDisabled, setIsSaveButtonDisabled] = useState(true);
+
+  let updateNotebook = useNotebooksStore((s) => s.updateNotebook);
+
+  useEffect(() => {
+    if (title === initialTitle && content === initialContent) {
+      setIsSaveButtonDisabled(true);
+      return;
+    }
+    if (isSaveButtonDisabled) {
+      setIsSaveButtonDisabled(false);
+    }
+  }, [title, content]);
+
+  const update = () => {
+    if (!notebook?.id)
+      throw new Error("Failed trying to update notebook. Invalid id.");
+    updateNotebook(notebook.id, { title, content });
+    setInitialTitle(title);
+    setInitialContent(content);
+    setIsSaveButtonDisabled(true);
+  };
 
   return (
     <div className="flex flex-col items-start gap-3">
@@ -33,6 +60,14 @@ const NotebookDetails = () => {
             value={content}
             onChange={(ev) => setContent(ev.target.value)}
           />
+          <Button
+            variant="outline"
+            color="dark"
+            disabled={isSaveButtonDisabled}
+            onClick={update}
+          >
+            Save
+          </Button>
         </>
       ) : (
         <div>No notebook with this id :(</div>
