19a77d85420f4ab913a477473e1a9656c7ae1731
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-04-02 22:58:54 +0330

    remove immer

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,35 @@
 import { create } from "zustand";
-import { immer } from "zustand/middleware/immer";
 import { Notebook } from "./types";
 import { notebooks } from "./mock-data";
 
 interface NotebooksState {
   notebooks: Notebook[];
-  updateNotebook: (updatedNotebook: Notebook) => void;
+
+  updateNotebook: (id: Notebook["id"], newData: Omit<Notebook, "id">) => void;
   addNotebook: (newNotebook: Notebook) => void;
 }
 
-export const useNotebooksStore = create<NotebooksState>()(
-  immer((set) => ({
-    notebooks,
-    updateNotebook(updatedNotebook) {},
-    addNotebook(newNotebook) {
-      set((s) => {
-        s.notebooks.unshift(newNotebook);
-      });
-    },
-  }))
-);
+export const useNotebooksStore = create<NotebooksState>()((set) => ({
+  notebooks,
+  updateNotebook(id, { title, content }) {
+    set((s) => {
+      let indexOfUpdated = s.notebooks.findIndex((n) => n.id === id);
+
+      if (indexOfUpdated === -1) {
+        throw new Error("The notebook you tried to update was not found");
+      }
+
+      return {
+        ...s,
+        notebooks: s.notebooks.map((n) =>
+          n.id === id ? { id: n.id, title, content } : n
+        ),
+      };
+    });
+  },
+  addNotebook(newNotebook) {
+    set((s) => {
+      return { ...s, notebooks: [newNotebook, ...s.notebooks] };
+    });
+  },
+}));
