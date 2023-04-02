37e78d71b66d29c709965bfa7801fa74549107f6
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-04-02 20:46:54 +0330

    add editable input

--- src/views/notebook-details.tsx
+++ src/views/notebook-details.tsx
@@ -1,23 +1,40 @@
-import { Button } from "@mantine/core";
+import { Button, TextInput, Textarea } from "@mantine/core";
 import { notebooks } from "../mock-data";
 import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
 
 const NotebookDetails = () => {
-  let params = useParams();
-  let navigate = useNavigate();
-  let nb = notebooks.find((n) => n.id === params.id);
+  const params = useParams();
+  const navigate = useNavigate();
+  const notebook = notebooks.find((n) => n.id === params.id);
+  const [content, setContent] = useState(notebook?.content ?? "");
+  const [title, setTitle] = useState(notebook?.title ?? "");
 
   return (
     <div className="flex flex-col items-start gap-3">
       <Button variant="outline" color="dark" onClick={() => navigate("/")}>
         go back
       </Button>
-      {!nb && <div>no nb</div>}
-      {nb && (
+      {notebook ? (
         <>
-          <h1 className="font-bold">{nb.title}</h1>
-          <p>{nb.content}</p>
+          <TextInput
+            placeholder="Title"
+            value={title}
+            className="w-full font-bold"
+            variant="unstyled"
+            onChange={(ev) => setTitle(ev.target.value)}
+          />
+          <TextareaAutosize
+            className="w-full resize-none outline-none"
+            maxRows={8}
+            placeholder="Notebook content"
+            value={content}
+            onChange={(ev) => setContent(ev.target.value)}
+          />
         </>
+      ) : (
+        <div>No notebook with this id :(</div>
       )}
     </div>
   );
