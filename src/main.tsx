447a9984bea27c3fe589702473c5f5acc0e6bac1
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-04-02 17:54:50 +0330

    apply some styles in dev mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Popup from "./Popup";
-import {Router} from "react-router-dom";
 import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+if (import.meta.env.DEV) document.body.classList.add("dev-mode");
+
+let root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <Popup />
   </React.StrictMode>
