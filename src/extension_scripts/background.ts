f90baa3d1cbe68a23b183a26d79c9008346877d8
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-04-07 17:50:57 +0330

    use esbuild for extension specific scripts

--- src/extension_scripts/background.ts
+++ src/extension_scripts/background.ts
@@ -1,12 +1,22 @@
-// Add a listener to create the initial context menu items,
-// context menu items only need to be created at runtime.onInstalled
-chrome.runtime.onInstalled.addListener(async () => {
-  [1, 2, 3].forEach((n) =>
-    chrome.contextMenus.create({
-      id: n.toString(),
-      title: n.toString(),
-      type: "normal",
-      contexts: ["selection"],
-    })
-  );
-});
+import { Notebook } from "../types";
+
+const getNotebooksFromStorage = async () =>
+  (await chrome.storage.local.get(["notebooks"])).notebooks;
+
+const main = () => {
+  // Add a listener to create the initial context menu items,
+  // context menu items only need to be created at runtime.onInstalled
+  chrome.runtime.onInstalled.addListener(async () => {
+    let notebooks: Notebook[] = await getNotebooksFromStorage();
+    for (const notebook of notebooks) {
+      chrome.contextMenus.create({
+        id: notebook.id,
+        title: notebook.title,
+        type: "normal",
+        contexts: ["selection"],
+      });
+    }
+  });
+};
+
+main();
