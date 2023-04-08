9dc1af15615bf0e387852ead8a87da7e2d94add0
Danial Zahmatkesh <work.dan.zah@gmail.com>
2023-04-08 13:41:23 +0430

    only problem now is the first start up

--- src/extension_scripts/background.ts
+++ src/extension_scripts/background.ts
@@ -14,11 +14,11 @@ const createMenuItem = (id: string, title: string) =>
     contexts: ["selection"],
   });
 
-const initiallizeMenuOnFirstInstall = () => {
+const initiallizeMenuOnFirstInstall = async () => {
   // Add a listener to create the initial context menu items,
   // context menu items only need to be created at runtime.onInstalled
-  chrome.runtime.onInstalled.addListener(async () => {
-    let notebooks: Notebook[] = await getNotebooksFromStorage();
+  let notebooks: Notebook[] = await getNotebooksFromStorage();
+  chrome.runtime.onInstalled.addListener(() => {
     for (const { id, title } of notebooks) {
       createMenuItem(id, title);
     }
@@ -46,30 +46,27 @@ const main = () => {
   addClickListenersToItems();
 
   // Add or removes the notebook title from context menu
-  // when the user updates notebooks
+  // when the user updates storage
   chrome.storage.onChanged.addListener(({ notebooks }) => {
     const currNotebooks: Notebook[] = notebooks.newValue;
     const prevNotebooks: Notebook[] = notebooks.oldValue ?? notebooks.newValue;
+    const isFirstStartup = notebooks.oldValue === undefined;
+    const allWereDeleted = notebooks.oldValue.length === 0;
     let notebookWasAdded = currNotebooks.length > prevNotebooks.length;
     let notebookWasRemoved = currNotebooks.length < prevNotebooks.length;
     if (notebookWasAdded) {
+      // because I always add them to the beginning of the notebooks array
       let { id, title } = currNotebooks[0];
       createMenuItem(id, title);
       return;
     }
     if (notebookWasRemoved) {
-      for (const nb of prevNotebooks) {
-        const deletedNotebook = currNotebooks.find((currNb) => {
-          const notebookStillExists = nb.id === currNb.id;
-          if (notebookStillExists) {
-            return false
-          } else {
-            return true
-          }
-
-        });
-        if (deletedNotebook) {
-          return chrome.contextMenus.remove(deletedNotebook.id);
+      for (const notebook of prevNotebooks) {
+        let isDeletedNotebook = !currNotebooks.some(
+          (currNotebook) => currNotebook.id === notebook.id
+        );
+        if (isDeletedNotebook) {
+          chrome.contextMenus.remove(notebook.id);
         }
       }
     }
