f87df7b934658e360a99e27c23d2b3ae3ff25cfe
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2023-04-27 23:50:22 +0330

    add basic search based on title

--- src/pages/notebooks.tsx
+++ src/pages/notebooks.tsx
@@ -1,25 +1,51 @@
-import { Button } from "@mantine/core";
+import { Button, TextInput, ActionIcon } from "@mantine/core";
 import NotebookCard from "../components/notebook-card";
 import { useNavigate } from "react-router-dom";
 import { useNotebooksStore } from "../store/notebooks-store";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+// @ts-ignore
+import IconSearch from "@tabler/icons-react/dist/esm/icons/IconSearch";
 
 const Notebooks = () => {
   const notebooks = useNotebooksStore((s) => s.notebooks);
   const retrieveNotebooks = useNotebooksStore((s) => s.retrieveNotebooks);
   let navigate = useNavigate();
+  let [searchString, setSearchString] = useState("");
+  let filteredNotebooks = useMemo(() => {
+    return notebooks.filter((n) => n.title.includes(searchString));
+  }, [notebooks, searchString]);
 
   useEffect(() => {
     retrieveNotebooks();
-  }, []);
+  }, [retrieveNotebooks]);
 
   return (
     <div className="flex flex-col items-start gap-3">
-      <h1 className="font-bold">Notebooks</h1>
+      <div className="flex w-full items-center justify-between gap-2">
+        <h1 className="font-bold">Notebooks</h1>
+        <TextInput
+          placeholder="fuzzy search"
+          size="xs"
+          value={searchString}
+          onChange={(e) => setSearchString(e.target.value)}
+          icon={<IconSearch size={"1rem"} />}
+        />
+      </div>
+
       <div className="flex max-h-64 w-full flex-col items-start gap-3 overflow-y-auto py-2">
-        {notebooks.map((n) => (
-          <NotebookCard notebook={n} key={n.id} />
-        ))}
+        {searchString !== "" ? (
+          <>
+            {filteredNotebooks.map((n) => (
+              <NotebookCard notebook={n} key={n.id} />
+            ))}
+          </>
+        ) : (
+          <>
+            {notebooks.map((n) => (
+              <NotebookCard notebook={n} key={n.id} />
+            ))}
+          </>
+        )}
       </div>
       <Button variant="outline" color="dark" onClick={() => navigate("/add")}>
         new notebook
